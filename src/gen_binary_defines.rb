41db7b52ea7f9d0899c0763b0421b0b07656a7ff
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 20:59:26 +0000

    change default width to 16

--- src/gen_binary_defines.rb
+++ src/gen_binary_defines.rb
@@ -9,7 +9,7 @@ File.open("BinaryDefines.h", "w+") do |f|
   f.puts "#ifndef BINARY_DEFINES_H
 #define BINARY_DEFINES_H"
   f.print "\n"
-  for i in 1..(ARGV[0].to_i || 2) do
+  for i in 1..(ARGV[0].to_i || 16) do
     f.print "#{def_bin_macro(i)}\n"
   end
   f.print "#define B(binary) B_##binary
