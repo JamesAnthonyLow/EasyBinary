f1f636b65ccee1fb099e1a4e471543e88829c419
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 20:11:48 +0000

    programatically write passing BinaryDefines.h for 2 testcases

--- src/BinaryDefines.h
+++ src/BinaryDefines.h
@@ -1,8 +1,8 @@
 #ifndef BINARY_DEFINES_H
 #define BINARY_DEFINES_H
 
-#define B_00000000 0
-#define B_1111 15
+#define B_0 0
+#define B_1 1
 #define B(binary) B_##binary
 
 #endif
--- src/gen_binary_defines.rb
+++ src/gen_binary_defines.rb
@@ -1,11 +1,16 @@
+def def_bin_macro(width, n)
+  "#define B_%0*d %d" % [width, n.to_s(2), n]
+end
+
 File.open("BinaryDefines.h", "w+") do |f|
-  f.print "#ifndef BINARY_DEFINES_H
-#define BINARY_DEFINES_H
-
-#define B_00000000 0
-#define B_1111 15
-
-#define B(binary) B_##binary
+  f.puts "#ifndef BINARY_DEFINES_H
+#define BINARY_DEFINES_H"
+  f.print "\n"
+  for i in 0..2**0 do
+    f.puts def_bin_macro(1, i)
+  end
+  f.print "\n"
+  f.print "#define B(binary) B_##binary
 
 #endif"
 end
