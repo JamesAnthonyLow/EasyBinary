8afcee1fde995889f4f066d6327c2c7c03b962fc
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 19:52:02 +0000

    add another test case

--- test/TestBinaryDefines.c
+++ test/TestBinaryDefines.c
@@ -5,6 +5,7 @@ void test_B_Macro_ConvertsFromBinary(void)
 {
   TEST_ASSERT_EQUAL_UINT16(0, B(00000000));
   TEST_ASSERT_EQUAL_UINT16(15, B(1111));
+  TEST_ASSERT_EQUAL_UINT16(45, B(101101));
 }
 
 int main(void)
