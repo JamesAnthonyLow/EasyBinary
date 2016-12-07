f89f041b6545aab7cef0a3dcde2e678d1492f2db
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 21:08:34 +0000

    passing tests up to width of 5

--- src/BinaryDefines.h
+++ src/BinaryDefines.h
@@ -73,6 +73,72 @@
 #define B_11111 31
 //Binary numbers of width 5
 
+#define B_000000 0
+#define B_000001 1
+#define B_000010 2
+#define B_000011 3
+#define B_000100 4
+#define B_000101 5
+#define B_000110 6
+#define B_000111 7
+#define B_001000 8
+#define B_001001 9
+#define B_001010 10
+#define B_001011 11
+#define B_001100 12
+#define B_001101 13
+#define B_001110 14
+#define B_001111 15
+#define B_010000 16
+#define B_010001 17
+#define B_010010 18
+#define B_010011 19
+#define B_010100 20
+#define B_010101 21
+#define B_010110 22
+#define B_010111 23
+#define B_011000 24
+#define B_011001 25
+#define B_011010 26
+#define B_011011 27
+#define B_011100 28
+#define B_011101 29
+#define B_011110 30
+#define B_011111 31
+#define B_100000 32
+#define B_100001 33
+#define B_100010 34
+#define B_100011 35
+#define B_100100 36
+#define B_100101 37
+#define B_100110 38
+#define B_100111 39
+#define B_101000 40
+#define B_101001 41
+#define B_101010 42
+#define B_101011 43
+#define B_101100 44
+#define B_101101 45
+#define B_101110 46
+#define B_101111 47
+#define B_110000 48
+#define B_110001 49
+#define B_110010 50
+#define B_110011 51
+#define B_110100 52
+#define B_110101 53
+#define B_110110 54
+#define B_110111 55
+#define B_111000 56
+#define B_111001 57
+#define B_111010 58
+#define B_111011 59
+#define B_111100 60
+#define B_111101 61
+#define B_111110 62
+#define B_111111 63
+//Binary numbers of width 6
+
 #define B(binary) B_##binary
 
 #endif
