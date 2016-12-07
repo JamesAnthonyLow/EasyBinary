6d3092ad0d566071aed20d70919394cf6694be49
JamesAnthonyLow <jamesalowenthal@gmail.com>
2016-12-07 19:49:30 +0000

    add ruby script to generate BinaryDefines.h

---
+++ src/gen_binary_defines.rb
@@ -0,0 +1,10 @@
+File.open("BinaryDefines.h", "w+") do |f|
+  f.print "#ifndef BINARY_DEFINES_H
+#define BINARY_DEFINES_H
+
+#define B_00000000 0
+#define B_1111 15
+#define B(binary) B_##binary
+
+#endif"
+end
